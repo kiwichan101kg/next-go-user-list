8ea9e86a5fc2a295bf231609b28d08eb3a2e8c54
kjnhk1001 <kjn.hk10001@gmail.com>
2024-02-10 19:35:25 +0900

    next.jsセットアップ

---
+++ user-list/src/app/user/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <form
+        action="
+    "
+      >
+        <label htmlFor="">名前</label>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default page;
